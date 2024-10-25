d5e66d56abb56c8f1e29dd8f8b918124ad4f08ee
Fred Lopes <fredlopes.priv@outlook.com>
2024-10-25 05:54:06 -0300

    inicial

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const background = document.querySelector('.background-codes');
+
+for (let i = 0; i < 100; i++) {
+    const codeColumn = document.createElement('div');
+    codeColumn.classList.add('code-column');
+    codeColumn.style.left = `${Math.random() * 100}vw`;
+    codeColumn.style.animationDuration = `${Math.random() * 2 + 3}s`;
+    background.appendChild(codeColumn);
+
+    for (let j = 0; j < 20; j++) {
+        const codeChar = document.createElement('span');
+        codeChar.textContent = Math.floor(Math.random() * 2);
+        codeColumn.appendChild(codeChar);
+    }
+}
